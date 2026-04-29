cff0ff3f07eb4fa5483dd156f757cfc260d5cd65
John Wolf <jwolf842@users.noreply.github.com>
2026-04-29 18:58:36 -0400

    Assign string lengths using .length property

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -26,5 +26,11 @@ let lengthThree; // Length of stringThree
 
 // Your code here
 
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
 
+console.log(lengthOne);   // 15
+console.log(lengthTwo);   // 10
+console.log(lengthThree); // 0
 
